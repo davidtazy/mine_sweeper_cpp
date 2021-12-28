fd6233efc595a62f609e4f2c8f86045273317ea6
davidtazy <david.allemant@gmail.com>
2021-12-28 21:57:44 +0100

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davidallemant/mine_sweeper_cpp
-# Build directory: /home/davidallemant/mine_sweeper_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_grid "test_generator")
-set_tests_properties(test_grid PROPERTIES  _BACKTRACE_TRIPLES "/home/davidallemant/mine_sweeper_cpp/CMakeLists.txt;38;add_test;/home/davidallemant/mine_sweeper_cpp/CMakeLists.txt;0;")
